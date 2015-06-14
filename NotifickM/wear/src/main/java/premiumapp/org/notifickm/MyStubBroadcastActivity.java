d841caf1135e19182d0e80b6c9502cf1b25c0aa0
ruan65 <ruan65@gmail.com>
2015-06-15 02:40:05 +0300

    initial

---
+++ NotifickM/wear/src/main/java/premiumapp/org/notifickm/MyStubBroadcastActivity.java
@@ -0,0 +1,22 @@
+package premiumapp.org.notifickm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("premiumapp.org.notifickm.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
