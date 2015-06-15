59c694bb32b64331d91b9ca35532ce4d7f79da9f
ruan65 <ruan65@gmail.com>
2015-06-15 03:43:18 +0300

    main activity created

---
+++ NotifickM/mobile/src/main/java/org/premiumapp/notifick/MainActivity.java
@@ -0,0 +1,14 @@
+package org.premiumapp.notifick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
